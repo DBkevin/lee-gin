acc3f672771032c81a9499b0d7ce3288e5d4bcf5
Lee@ubn <qq416606903@hotmail.com>
2024-04-15 16:37:11 +0800

    jwt 初始化

--- config/config.go
+++ config/config.go
@@ -4,4 +4,5 @@ type Configuation struct {
 	App      App      `mapstructure:"app" json:"app" yaml:"app"`
 	Log      Log      `mapstructure:"log" josn:"log" yaml:"log"`
 	Database Database `mapstructure:"database" json:"database" yaml:"database"`
+	Jwt      Jwt      `mapstructure:"jwt" json:"jwt" yaml:"jwt"`
 }
